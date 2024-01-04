72b64faa0887d5b02cdec5deb75d10a620c245e2
smomara <omarams@cua.edu>
2024-01-04 17:26:55 -0500

    Day 11

--- python/11.py
+++ python/11.py
@@ -1,64 +1,53 @@
-from typing import List, Tuple
+from typing import List
+from itertools import combinations
 
 INPUT = [line.strip() for line in open('input/11.txt').readlines()]
 
 class Map:
-    def __init__(self, lines: List[str]):
-        self.lines: List[str] = lines
-        self.galaxies: List[Tuple[int, int]] = self._find_galaxies()
+    def __init__(self):
+        self.lines: List[str] = []
+        self.galaxies: List[List[int]] = self._find_galaxies()
 
-    def _find_galaxies(self) -> List[Tuple[int, int]]:
-        return [(x, y) for x, row in enumerate(self.lines) for y, c in enumerate(row) if c != '.']
+    def _find_galaxies(self) -> List[List[int]]:
+        return [[x, y] for y, row in enumerate(self.lines) for x, c in enumerate(row) if c != '.']
+    
+    @classmethod
+    def expand_galaxy(cls, lines: List[str], years: int) -> "Map":
+        map = cls()
+        map.lines = lines
+        map.galaxies = map._find_galaxies()
 
-    # TODO optimize for large numbers
-    def expand_galaxy(self, years: int) -> None:
-        # identify and expand rows
-        expanded_lines = []
-        for y, row in enumerate(self.lines):
-            if "#" not in row:
-                expanded_lines.extend(['.' * len(row)] * years)
-            expanded_lines.append(row)
-        self.lines = expanded_lines
+        years -=1
+
+        rows, cols = len(map.lines), len(map.lines[0])
+        row_expansion = [0] * rows
+        col_expansion = [0] * cols
+
+        for y in range(rows):
+            row_expansion[y] = row_expansion[y-1] + (years if set(lines[y]) == {'.'} else 0)
+        for x in range(cols):
+            col_expansion[x] = col_expansion[x-1] + (years if all(lines[y][x] == '.' for y in range(rows)) else 0)
         
-        # identify columns
-        row_length = len(self.lines[0])
-        columns_to_expand = [True] * row_length
+        for galaxy in map.galaxies:
+            galaxy[0] += col_expansion[galaxy[0]]
+            galaxy[1] += row_expansion[galaxy[1]]
 
-        for row in self.lines:
-            for x, c in enumerate(row):
-                if c == '#':
-                    columns_to_expand[x] = False
-                    continue
-
-        # expand identified columns
-        for y in range(len(self.lines)):
-            self.lines[y] = ''.join([c + ('.' * years if columns_to_expand[x] else '') for x, c in enumerate(self.lines[y])])
-        
-        # update galaxy location
-        self.galaxies = self._find_galaxies()
+        return map
 
 def eval(map: Map) -> int:
-    total = 0
-    for i in range(len(map.galaxies)-1):
-        for j in range(i+1, len(map.galaxies)):
-            dist = abs(map.galaxies[i][0] - map.galaxies[j][0]) + abs(map.galaxies[i][1] - map.galaxies[j][1])
-            total += dist
-    return total
+    return sum(abs(x[0] - y[0]) + abs(x[1] - y[1]) for x, y in combinations(map.galaxies, 2))
 
 def part1(lines: List[str]) -> int:
-    map = Map(lines)
-    map.expand_galaxy(1)
+    map = Map().expand_galaxy(lines, 2)
     return eval(map)
 
-
 def part2(lines: List[str]) -> int:
-    map = Map(lines)
-    map.expand_galaxy(1000000)
+    map = Map().expand_galaxy(lines, 1000000)
     return eval(map)
 
 def main() -> None:
     print(f"Part 1: {part1(INPUT)}")
-    #print(f"Part 2: {part2(INPUT)}")
+    print(f"Part 2: {part2(INPUT)}")
 
 if __name__ == "__main__":
     main()
