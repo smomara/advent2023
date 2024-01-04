253ed3ae21c1fb651c7c38ecdcbeea02b0780b09
smomara <omarams@cua.edu>
2024-01-03 20:52:02 -0500

    Day 9

--- python/9.py
+++ python/9.py
@@ -5,21 +5,32 @@ INPUT = [line.strip() for line in open('input/9.txt').readlines()]
 def parse(lines: List[str]) -> List[List[int]]:
         return [[[int(x) for x in line.split(" ")]] for line in lines]
 
-def eval(history: List[int]) -> int:
+def eval1(history: List[int]) -> int:
     while sum(history[-1]) != 0:
         history.append([r - l for l, r in zip(history[-1], history[-1][1:])])
     return sum([line[-1] for line in history])
 
-def part1(histories: List[List[int]]) -> int:
-    return sum([eval(history) for history in histories])
+def eval2(history: List[int]) -> int:
+    # Generate new lines until all 0
+    while sum(history[-1]) != 0:
+        history.append([r - l for l, r in zip(history[-1], history[-1][1:])])
+   
+    # Reconstruct history in reverse
+    for i in range(len(history) - 2, -1, -1):
+        history[i].insert(0, history[i][0] - history[i+1][0])
 
-def part2(lines: List[str]) -> int:
-    return 0
+    return history[0][0]
+
+def part1(histories: List[List[int]]) -> int:
+    return sum([eval1(history) for history in histories])
+
+def part2(histories: List[List[int]]) -> int:
+    return sum([eval2(history) for history in histories])
 
 def main() -> None:
     histories = parse(INPUT)
     print(f"Part 1: {part1(histories)}")
-    print(f"Part 2: {part2(INPUT)}")
+    print(f"Part 2: {part2(histories)}")
 
 if __name__ == "__main__":
     main()
